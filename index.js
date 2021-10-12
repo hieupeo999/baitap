eb5d3036fa2b25f398ffd1daea014b4a6a7cca51
hieupeo99 <minhhieubg99@gmail.com>
2021-10-12 14:19:27 +0700

    minhhieu

---
+++ index.js
@@ -0,0 +1,24 @@
+switch (new ontimeupdate().getDay()){
+    case 0;
+        day = "sunday";
+        break;
+   case 1;
+    day = "monday";
+    break;
+   case 2;
+    day = "tuesday";
+    break;
+   case 3;
+    day = "wednesday";
+    break;
+   case 4;
+    day = "thursday";
+    break;
+   case 5;
+    day = "friday";
+    break;
+    case 6;
+    day ="saturday";
+    break;
+
+}
